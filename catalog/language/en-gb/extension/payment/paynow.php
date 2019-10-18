fe2bb3eaf543e616bb49b15b1ae8523357dd7de5
Agus Suarya <agus.suarya@spinroam.com>
2019-10-18 15:57:39 +0800

    Centered image.

--- catalog/language/en-gb/extension/payment/paynow.php
+++ catalog/language/en-gb/extension/payment/paynow.php
@@ -1,4 +1,4 @@
 <?php
 
-$_['text_title'] = 'Paynow <img src="catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="margin-left: 5px;"/>';
+$_['text_title'] = 'Paynow <img src="catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="margin-left: 5px; position: relative; top: -2px;"/>';
 $_['button_confirm'] = 'Confirm Order';
