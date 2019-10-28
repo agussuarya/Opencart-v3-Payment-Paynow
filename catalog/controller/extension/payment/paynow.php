68715387dc84989e436e62bfcfc6c87fb807df2d
Agus Suarya <agus.suarya@spinroam.com>
2019-10-28 14:09:25 +0800

    Add instruction.

--- catalog/controller/extension/payment/paynow.php
+++ catalog/controller/extension/payment/paynow.php
@@ -4,18 +4,44 @@ class ControllerExtensionPaymentPayNow extends Controller {
     public function index() {
         $this->language->load('extension/payment/paynow');
 
+        if ($this->request->server['HTTPS']) {
+            $data['store_url'] = HTTPS_SERVER;
+        } else {
+            $data['store_url'] = HTTP_SERVER;
+        }
+
         $data['button_confirm'] = $this->language->get('button_confirm');
 
         $data['action'] = 'https://yourpaymentgatewayurl';
 
+        // Get order info
         $this->load->model('checkout/order');
-
         $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
 
+        // Get order billing country
+        $this->load->model('localisation/country');
+        $country_info = $this->model_localisation_country->getCountry($order_info['payment_country_id']);
+
+        // We will use this owner info to send Paynow from client side
+        $data['billing_details'] = array(
+            'billing_details' => array(
+                'name' => $order_info['payment_firstname'] . ' ' . $order_info['payment_lastname'],
+                'email' => $order_info['email'],
+                'address' => array(
+                    'line1'	=> $order_info['payment_address_1'],
+                    'line2'	=> $order_info['payment_address_2'],
+                    'city'	=> $order_info['payment_city'],
+                    'state'	=> $order_info['payment_zone'],
+                    'postal_code' => $order_info['payment_postcode'],
+                    'country' => $country_info['iso_code_2']
+                )
+            )
+        );
+
         if ($order_info) {
-            $data['text_config_one'] = trim($this->config->get('text_config_one'));
-            $data['text_config_two'] = trim($this->config->get('text_config_two'));
-            $data['orderid'] = date('His') . $this->session->data['order_id'];
+            $data['text_config_one'] = trim($this->config->get('payment_paynow_config_one'));
+            $data['text_config_two'] = trim($this->config->get('payment_paynow_config_two'));
+            $data['orderid'] = date('His') . $order_info['order_id'];
             $data['callbackurl'] = $this->url->link('payment/paynow/callback');
             $data['orderdate'] = date('YmdHis');
             $data['currency'] = $order_info['currency_code'];
@@ -36,12 +62,10 @@ class ControllerExtensionPaymentPayNow extends Controller {
             $data['deliveryphone'] = html_entity_decode($order_info['telephone'], ENT_QUOTES, 'UTF-8');
             $data['deliverypost'] = html_entity_decode($order_info['shipping_postcode'], ENT_QUOTES, 'UTF-8');
 
-            if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/extension/payment/paynow')){
-                return $this->load->view( $this->config->get('config_template' ) . '/template/extension/payment/paynow',
-                    $data );
-            } else {
-                return $this->load->view( 'extension/payment/paynow', $data );
-            }
+            // Generate barcode
+            $data['barcode_paynow'] = 'Yasa test';
+
+            return $this->load->view( 'extension/payment/paynow', $data );
         }
     }
 
@@ -64,4 +88,27 @@ class ControllerExtensionPaymentPayNow extends Controller {
             }
         }
     }
+
+    public function confirm() {
+        $json = array();
+
+        if ($this->session->data['payment_method']['code'] == 'paynow') {
+            $this->load->language('extension/payment/paynow');
+
+            $this->load->model('checkout/order');
+
+            $comment  = $this->language->get('text_instruction') . ":\n\n";
+            $comment .= $this->language->get('text_description_order_history') . "\n\n";
+            $comment .= "<img id='barcode' src='https://api.qrserver.com/v1/create-qr-code/?data=xxx&amp;size=100x100' alt='Scan this barcode to pay' title='Scan this barcode to pay' width='100' height='100' />\n\n";
+            $comment .= $this->language->get('text_payment');
+
+            $this->model_checkout_order->addOrderHistory($this->session->data['order_id'], $this->config->get('payment_paynow_order_success_status_id'), $comment, true);
+
+            $json['redirect'] = $this->url->link('checkout/success');
+        }
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+
 }
--- catalog/model/extension/payment/paynow.php
+++ catalog/model/extension/payment/paynow.php
@@ -3,12 +3,49 @@ class ModelExtensionPaymentPayNow extends Model {
     public function getMethod($address, $total) {
         $this->load->language('extension/payment/paynow');
 
-        $method_data = array(
-            'code'     => 'paynow',
-            'title'    => $this->language->get('text_title'),
-            'sort_order' => $this->config->get('paynow_sort_order')
-        );
+        $status = true;
+
+        // Paynow does not allow payment for 0 amount
+        if($total <= 0) {
+            $status = false;
+        }
+
+        $method_data = array();
+
+        if ($status) {
+            $method_data = array(
+                'code'       => 'paynow',
+                'title'      => $this->language->get('text_title'),
+                'terms'      => '',
+                'sort_order' => $this->config->get('payment_paynow_sort_order')
+            );
+        }
 
         return $method_data;
+    }
+
+    public function log($file, $line, $caption, $message){
+
+        if(!$this->config->get('payment_paynow_debug')){
+            return;
+        }
+
+        $iso_time = date('c');
+        $filename = 'paynow-'.strstr($iso_time, 'T', true).'.log';
+
+        $log = new Log($filename);
+        $msg = "[" . $iso_time . "] ";
+        $msg .= "<" . $file . "> ";
+        $msg .= "#" . $line . "# ";
+        $msg .= "~" . $caption . "~ ";
+
+        if(is_array($message)){
+            $msg .= print_r($message, true);
+        } else {
+            $msg .= PHP_EOL . $message;
+        }
+
+        $msg .= PHP_EOL . PHP_EOL;
+        $log->write($msg);
     }
 }
