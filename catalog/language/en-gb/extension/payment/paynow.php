3234ba8b0c4d55306319c9c1733ca9fa2eb32ea4
Agus Suarya <agus.suarya@spinroam.com>
2019-10-29 17:16:26 +0800

    Change logo.

--- catalog/language/en-gb/extension/payment/paynow.php
+++ catalog/language/en-gb/extension/payment/paynow.php
@@ -2,7 +2,7 @@
 
 $_['text_title']                     = 'Paynow <img src="catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="margin-left: 5px; position: relative; top: -2px;"/>';
 $_['button_confirm']                 = "Confirm";
-$_['text_instruction']               = 'Paynow Instructions';
+$_['text_instruction']               = 'Paynow Instructions <img src="catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="position: relative; top: -2px;"/>';
 $_['text_description']               = "Please scan the qr code below and then press <b>Confirm</b> if you already sent money.";
 $_['text_description_order_history'] = "Please scan the qr code & sent money for make payment.";
 $_['text_payment']                   = 'Your order will not ship until we receive payment.';
