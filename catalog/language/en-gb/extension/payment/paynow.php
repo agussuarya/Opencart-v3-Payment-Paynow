6f33fe025c16d6c7da939735edbd4900e0f8ca1b
Agus Suarya <agus.suarya@spinroam.com>
2019-10-29 17:11:39 +0800

    Change language.

--- catalog/language/en-gb/extension/payment/paynow.php
+++ catalog/language/en-gb/extension/payment/paynow.php
@@ -1,8 +1,8 @@
 <?php
 
 $_['text_title']                     = 'Paynow <img src="catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="margin-left: 5px; position: relative; top: -2px;"/>';
-$_['button_confirm']                 = "I've sent money";
+$_['button_confirm']                 = "Confirm";
 $_['text_instruction']               = 'Paynow Instructions';
-$_['text_description']               = "Please scan the qr code below and then press <b>I've sent money</b> if you already sent money.";
+$_['text_description']               = "Please scan the qr code below and then press <b>Confirm</b> if you already sent money.";
 $_['text_description_order_history'] = "Please scan the qr code & sent money for make payment.";
 $_['text_payment']                   = 'Your order will not ship until we receive payment.';
