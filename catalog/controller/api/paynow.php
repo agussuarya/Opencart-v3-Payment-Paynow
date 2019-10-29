ecf0adf7ef8c964126283a7aeb447c35760586eb
Agus Suarya <agus.suarya@spinroam.com>
2019-10-29 17:12:54 +0800

    API callback.

--- catalog/controller/api/paynow.php
+++ catalog/controller/api/paynow.php
@@ -1,11 +1,37 @@
 <?php
 class ControllerApiPaynow extends Controller
 {
+    /**
+     * Callback from paynow
+     */
     public function staging()
     {
-        $json = [
-            'a' => 'b'
-        ];
+        // Get data
+        $orderId = array_key_exists('order_id', $_POST) ? $_POST['order_id'] : 0;
+        $status = array_key_exists('status', $_POST) ? $_POST['status'] : 'no-action';
+        $json = [];
+
+        // Security ongoing :)
+        $canAccess = true;
+
+        if (!$canAccess) {
+            $json['error']['warning'] = 'Hmm...';
+        } else {
+            // Process if order id exists
+            if ($orderId == 0 || $status == 'no-action') {
+                $json['error']['warning'] = 'Hmm...';
+            } else {
+                // Process
+                $this->load->model('checkout/order');
+                $comment = "Successfully paid";
+                $newStatus = $status == 'paid' ? $this->config->get('payment_paynow_order_success_status_id') : $this->config->get('payment_paynow_order_failed_status_id');
+                $this->model_checkout_order->addOrderHistory($orderId, $newStatus, $comment, true);
+
+                $json['redirect'] = $this->url->link('checkout/success');
+                $json['message'] = 'Success.';
+            }
+        }
+
         $this->response->addHeader('Content-Type: application/json');
         $this->response->setOutput(json_encode($json));
     }
