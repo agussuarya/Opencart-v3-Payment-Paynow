bb8758d6a3f1d3314902aef8e61f6ec9206f8256
Agus Suarya <agus.suarya@spinroam.com>
2019-11-28 09:57:07 +0800

    Add create and delete pending order paynow model.

--- catalog/model/extension/payment/paynow.php
+++ catalog/model/extension/payment/paynow.php
@@ -1,5 +1,7 @@
 <?php
 class ModelExtensionPaymentPayNow extends Model {
+    private $TABLE_PAYNOW_PENDING_ORDER = DB_PREFIX . "order_pending_by_paynow";
+
     public function getMethod($address, $total) {
         $this->load->language('extension/payment/paynow');
 
@@ -47,5 +49,47 @@ class ModelExtensionPaymentPayNow extends Model {
 
         $msg .= PHP_EOL . PHP_EOL;
         $log->write($msg);
+    }
+
+    public function addPendingOrder($orderId)
+    {
+        // Get data order details
+        $this->load->model('checkout/order');
+        $order = $this->model_checkout_order->getOrder($orderId);
+
+        // Store pending order
+        if ($order) {
+            $this->db->query("INSERT INTO " . $this->TABLE_PAYNOW_PENDING_ORDER . " SET order_id = '" . (int)$order['order_id'] . "', total = '" . $order['total'] . "', currency_id = '" . (int)$order['currency_id'] . "', currency_code = '" . $order['currency_code'] . "', currency_value = '" . $order['currency_value'] . "', date_added = NOW(), date_modified = NOW()");
+        }
+    }
+
+    public function deletePendingOrder($orderId)
+    {
+        $this->db->query("DELETE FROM " . $this->TABLE_PAYNOW_PENDING_ORDER . " WHERE order_id = '" . (int)$orderId . "'");
+    }
+
+    /**
+     * Run this sql query
+     */
+    private function importSqlTable()
+    {
+        $this->db->query("
+            CREATE TABLE `oc_order_pending_by_paynow` (
+                `order_id` int(11) NOT NULL AUTO_INCREMENT,
+              `total` decimal(15,4) NOT NULL DEFAULT '0.0000',
+              `currency_id` int(11) NOT NULL,
+              `currency_code` varchar(3) NOT NULL,
+              `currency_value` decimal(15,8) NOT NULL DEFAULT '1.00000000',
+              `date_added` datetime NOT NULL,
+              `date_modified` datetime NOT NULL,
+              PRIMARY KEY (`order_id`)
+            ) ENGINE=MyISAM AUTO_INCREMENT=171 DEFAULT CHARSET=utf8;
+        ");
+
+    }
+
+    public function getTableName()
+    {
+        return $this->TABLE_PAYNOW_PENDING_ORDER;
     }
 }
