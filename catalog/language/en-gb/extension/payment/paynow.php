b7ecd0f565864e260e96ea2929ca0757911796ba
Agus Suarya <agus.suarya@spinroam.com>
2019-11-28 17:17:43 +0800

    Fix path paynow logo.

--- catalog/language/en-gb/extension/payment/paynow.php
+++ catalog/language/en-gb/extension/payment/paynow.php
@@ -1,8 +1,8 @@
 <?php
 
-$_['text_title']                             = 'Paynow <img src="catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="margin-left: 5px; position: relative; top: -2px;"/>';
+$_['text_title']                             = 'Paynow <img src="/catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="margin-left: 5px; position: relative; top: -2px;"/>';
 $_['button_confirm']                         = "Confirm";
-$_['text_instruction']                       = 'Paynow Instructions <img src="catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="position: relative; top: -2px;"/>';
+$_['text_instruction']                       = 'Paynow Instructions <img src="/catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="position: relative; top: -2px;"/>';
 $_['text_description']                       = "1. Click <b>Confirm</b> button below to confirm your order.<br/>&nbsp&nbsp&nbsp Once confirm button clicked, we directly generating your Paynow Qr Code. Please wait a few moments.<br/>2. <b>Scan</b> the Paynow Qr Code and make payment.<br/>3. When payment received, we will ship your order.";
 
 $_['text_description_order_history_1']       = "Please wait, a PayNow QR code is being generated.";
