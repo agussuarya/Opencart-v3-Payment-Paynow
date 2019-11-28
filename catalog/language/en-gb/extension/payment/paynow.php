dde65b04be8f0cea12145dfbaa45706344dc5759
Agus Suarya <agus.suarya@spinroam.com>
2019-11-28 09:54:40 +0800

    Add language.

--- catalog/language/en-gb/extension/payment/paynow.php
+++ catalog/language/en-gb/extension/payment/paynow.php
@@ -1,8 +1,15 @@
 <?php
 
-$_['text_title']                     = 'Paynow <img src="catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="margin-left: 5px; position: relative; top: -2px;"/>';
-$_['button_confirm']                 = "Confirm";
-$_['text_instruction']               = 'Paynow Instructions <img src="catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="position: relative; top: -2px;"/>';
-$_['text_description']               = "Please scan the qr code below and then press <b>Confirm</b> if you already sent money.";
-$_['text_description_order_history'] = "Please scan the qr code & sent money for make payment.";
-$_['text_payment']                   = 'Your order will not ship until we receive payment.';
+$_['text_title']                             = 'Paynow <img src="catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="margin-left: 5px; position: relative; top: -2px;"/>';
+$_['button_confirm']                         = "Confirm";
+$_['text_instruction']                       = 'Paynow Instructions <img src="catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="position: relative; top: -2px;"/>';
+$_['text_description']                       = "1. Click <b>Confirm</b> button below to confirm your order.<br/>&nbsp&nbsp&nbsp Once confirm button clicked, we directly generating your Paynow Qr Code. Please wait a few moments.<br/>2. <b>Scan</b> the Paynow Qr Code and make payment.<br/>3. When payment received, we will ship your order.";
+
+$_['text_description_order_history_1']       = "Please wait, a PayNow QR code is being generated.";
+$_['text_description_order_history_2']       = "<b>Scan</b> the Paynow Qr Code below and make payment.<br/>When payment received, we will ship your order.";
+$_['text_payment']                           = 'Your order will not ship until we receive payment.';
+
+$_['text_loading']                           = 'Processing Order';
+
+$_['text_description_order_history_success'] = "Successful paid via PayNow. Please wait we're processing your order.";
+$_['text_description_order_history_failed']  = "Your order has been cancelled.";
