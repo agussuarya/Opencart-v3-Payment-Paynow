2806be41ea6a63aa1e155d05a1b21749dddba740
Agus Suarya <agus.suarya@spinroam.com>
2019-12-12 09:55:41 +0800

    Change language instructions.

--- catalog/language/en-gb/extension/payment/paynow.php
+++ catalog/language/en-gb/extension/payment/paynow.php
@@ -2,12 +2,16 @@
 
 $_['text_title']                             = 'Paynow <img src="/catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="margin-left: 5px; position: relative; top: -2px;"/>';
 $_['button_confirm']                         = "Confirm";
-$_['text_instruction']                       = 'Paynow Instructions <img src="/catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="position: relative; top: -2px;"/>';
-$_['text_description']                       = "1. Click <b>Confirm</b> button below to confirm your order.<br/>&nbsp&nbsp&nbsp Once confirm button clicked, we directly generating your Paynow Qr Code. Please wait a few moments.<br/>2. <b>Scan</b> the Paynow Qr Code and make payment.<br/>3. When payment received, we will ship your order.";
+//$_['text_instruction']                       = 'Paynow Instructions <img src="/catalog/view/theme/default/image/paynow_logo.png" alt="Paynow" style="position: relative; top: -2px;"/>';
+$_['text_instruction']                       = 'PayNow Instructions';
+//$_['text_description']                       = "1. Click <b>Confirm</b> button below to confirm your order.<br/>&nbsp&nbsp&nbsp Once confirm button clicked, we directly generating your Paynow Qr Code. Please wait a few moments.<br/>2. <b>Scan</b> the Paynow Qr Code and make payment.<br/>3. When payment received, we will ship your order.";
+$_['text_description']                       = "1. Click the <b>Confirm</b> button to continue your order, a QR code will be generated.<br/>2. <b>Scan</b> the QR Code to continue payment.<br/>3. Your order will be processed when payment has been received.";
 
 $_['text_description_order_history_1']       = "Please wait, a PayNow QR code is being generated.";
-$_['text_description_order_history_2']       = "<b>Scan</b> the Paynow Qr Code below and make payment.<br/>When payment received, we will ship your order.";
-$_['text_payment']                           = 'Your order will not ship until we receive payment.';
+//$_['text_description_order_history_2']       = "<b>Scan</b> the Paynow Qr Code below and make payment.<br/>When payment received, we will ship your order.";
+$_['text_description_order_history_2']       = "Scan the PayNow QR code below to continue payment. We will process your order when payment has been received.";
+//$_['text_payment']                           = 'Your order will not ship until we receive payment.';
+$_['text_payment']                           = '';
 
 $_['text_loading']                           = 'Processing Order';
 
